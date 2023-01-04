4b2654bb67bd0903a35deb7231742c8458b266fd
junealexis.santos13@gmail.com <junealexis.santos13@gmail.com>
2023-01-04 16:18:00 +0800

    added image resources

---
+++ backend/backend.py
@@ -0,0 +1,13 @@
+import genshinstats as gs
+import pandas as pd
+
+#token ID and login ID
+
+gs.set_cookie(ltuid=69270783, ltoken="RFTYzxIugEIsan7EUeCguSSHld8cdvI4XPacp81i") # search all browsers
+
+uid = 811219969
+
+data = gs.get_user_stats(uid)['characters']
+print(data)
+# for x in data:
+#     print(x['name'], x['level'])
